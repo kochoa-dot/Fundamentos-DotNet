087c737d72b2d80370c6eb56b6f40e0e73e5127d
kochoa-dot <70181138+kochoa-dot@users.noreply.github.com>
2023-09-12 16:47:01 -0600

    End of the course

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Humanizer;
+
+Console.WriteLine("Por favor ingrese un nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Ingrese su edad");
+var edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Mi nombre es  {nombre}, mi cargo es {cargo} y tengo {edad.ToWords(new System.Globalization.CultureInfo("en"))} años" );
